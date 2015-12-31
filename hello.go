26061e528f1eab5821cf867d45c6556000bbfe70
Nathan Guenther <nathanpguenther@gmail.com>
2015-12-30 20:37:57 -0500

    Create hello.go

---
+++ hello.go
@@ -0,0 +1,14 @@
+package myapp
+
+import (
+    "fmt"
+    "net/http"
+)
+
+func init() {
+    http.HandleFunc("/", handler)
+}
+
+func handler(w http.ResponseWriter, r *http.Request) {
+    fmt.Fprint(w, "Hello, world! We making moves :O")
+}
