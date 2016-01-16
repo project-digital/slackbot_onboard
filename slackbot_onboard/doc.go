b0bbc7ea2ed839b33292f669152e9992d634923f
Nathan Guenther <nathanpguenther@gmail.com>
2016-01-16 14:25:46 -0500

    Update doc.go
    Fix go doc syntax

--- slackbot_onboard/doc.go
+++ slackbot_onboard/doc.go
@@ -1,3 +1,3 @@
-// Placeholder for future documentation
+// Package slackbot_onboard (Placeholder for future documentation)/
 // Via this format godoc will auto-generate and display the documenation online.
 package slackbot_onboard // import "golang.org/x/tools/godoc" // <-- Will update to our URL once live
