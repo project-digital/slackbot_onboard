09be2a5c1151804c561265c952b6c7df79eacb62
Nathan Guenther <nathanpguenther@gmail.com>
2016-01-16 14:56:00 -0500

    Update doc.go

--- slackbot_onboard/doc.go
+++ slackbot_onboard/doc.go
@@ -1,8 +1,8 @@
 // Package slackbot_onboard (Placeholder for future documentation)/
 // Via this format godoc will auto-generate and display the documenation online.
-package slackbot_onboard // import "golang.org/x/tools/godoc" // <-- Will update to our URL once live
+package doc
 
 import (
-        "fmt"
-        "github.com/project-digital/slackbot_onboard"
+	"fmt"
+	"github.com/project-digital/slackbot_onboard"
 )
