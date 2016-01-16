340ac8fdc0c84cd939797b9e147ba96f5d128e37
Nathan Guenther <nathanpguenther@gmail.com>
2016-01-16 15:24:38 -0500

    Move main package in root

--- main.go
+++ main.go
@@ -1,6 +1,6 @@
 // Package slackbot_onboard (Placeholder for future documentation)/
 // Via this format godoc will auto-generate and display the documenation online.
-package slackbot_onboard
+package main 
 
 import (
 	"fmt"
