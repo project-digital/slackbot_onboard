4ba2cd802cd1f772c3cdc857d485426c9aa482b3
Joseph Landry <joelandry80@gmail.com>
2016-01-18 00:19:06 -0500

    update test.js

--- test.js
+++ test.js
@@ -82,18 +82,20 @@ var bot = controller.spawn({
 
 controller.on('bot_channel_join',function(bot,message){
     bot.startConversation(message, function(err, convo){
+      
+      convo.ask(message,"Welcome to the OnBoarding Bot. To get started, what is your business name? (To stop setup at anytime, type 'end')",[
+            {
+              callback: function(response, convo){
+                convo.say("Great, so your company is: " + response.text);
+                convo.next();
+              }
+            }
+        ]);
       convo.on('end', function(convo){
           if(convo.status =='active'){
             convo.stop();
           }
       });
-      convo.ask(message,"Welcome to the OnBoarding Bot. To get started, what is your business name? (To stop setup at anytime, type 'end')",[
-            {
-              callback: function(response, convo){
-                convo.say("Great, so your company is: " + response);
-              }
-            }
-        ]);
     });
 });
 
