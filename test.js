093b162a814fbea06a1e25f6f8db794cb3d2ca9b
Joseph Landry <joelandry80@gmail.com>
2016-01-17 23:39:40 -0500

    update test.js

--- test.js
+++ test.js
@@ -73,13 +73,16 @@ var Botkit = require('./lib/Botkit.js');
 var os = require('os');
 
 var controller = Botkit.slackbot({
-    debug: true,
+    debug: false,
 });
 
 var bot = controller.spawn({
     token: process.env.token
 }).startRTM();
 
+controller.on('im_created',function(bot,message){
+  bot.reply(message,"Welcome to the bot");
+})
 
 controller.hears(['hello','hi'],'direct_message,direct_mention,mention',function(bot, message) {
 
